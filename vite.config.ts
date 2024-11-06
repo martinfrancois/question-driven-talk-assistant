8ffd07515a0e102f6aefa7a4e71e9b0899ea2227
martinfrancois <f.martin@fastmail.com>
2024-11-06 12:38:40 +0100

    Switch to vite react PWA
    Generated using: https://vite-pwa-org.netlify.app/guide/#scaffolding-your-first-vite-pwa-project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Question-Driven Talk Assistant',
+      short_name: 'TalkAssistant',
+      description: 'Web app to use in conference talks focused on answering questions from the audience.',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
