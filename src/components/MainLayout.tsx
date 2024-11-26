ce6aaddfc46689a451bebecf77b9b3fd2305dc38
martinfrancois <f.martin@fastmail.com>
2024-11-26 21:27:26 +0100

    Move dark mode related classes from `MainLayout` up to `App`

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,24 +3,17 @@ import { Header } from "./Header.tsx";
 import { MainContent } from "./MainContent.tsx";
 import { Footer } from "./Footer.tsx";
 import { FullScreenQrCode } from "./FullScreenQrCode.tsx";
-import { useDarkMode } from "../stores";
 
-const MainLayout: FC = () => {
-  const isDarkMode = useDarkMode();
-
-  return (
-    <div
-      className={`flex h-screen w-screen flex-col p-4 ${
-        isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black" // TODO can be moved to App?
-      } ${isDarkMode ? "dark" : "light"}`} // TODO necessary?
-      data-testid="main-layout-container"
-    >
-      <Header />
-      <MainContent />
-      <Footer />
-      <FullScreenQrCode />
-    </div>
-  );
-};
+const MainLayout: FC = () => (
+  <div
+    className={`flex h-screen w-screen flex-col p-4`}
+    data-testid="main-layout-container"
+  >
+    <Header />
+    <MainContent />
+    <Footer />
+    <FullScreenQrCode />
+  </div>
+);
 
 export default MainLayout;
