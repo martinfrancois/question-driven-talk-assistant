19af2a2a0eaaeeea342de2796e0c5e93d8ec89f2
martinfrancois <f.martin@fastmail.com>
2024-12-01 14:44:59 +0100

    Add shortcuts to edit title and footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,6 @@
 import { useFooter, useSetFooter } from "../../stores";
 import { useCallback } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 
 export function Footer() {
   const footer = useFooter();
@@ -10,12 +11,19 @@ export function Footer() {
     if (newFooter !== null) setFooter(newFooter);
   }, [footer, setFooter]);
 
+  useHotkeys("ctrl+shift+f", editFooter, { enableOnFormTags: true }, [
+    editFooter,
+  ]);
+
   return (
     <footer
       onClick={editFooter}
       className="flex-shrink-0 cursor-pointer p-2 text-center text-xl"
       data-testid="main-footer"
-      aria-label={"Footer text: " + (footer ?? "empty")}
+      aria-label={
+        "Footer text: " + (footer ?? "empty") + ". Click to edit footer."
+      }
+      aria-keyshortcuts="Control+Shift+F to edit footer text"
     >
       {footer}
     </footer>
