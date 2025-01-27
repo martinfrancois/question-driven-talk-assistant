4f8ce99c9a4475cffa5075f423a98e88ed7d8aa2
martinfrancois <f.martin@fastmail.com>
2025-01-27 12:48:08 +0100

    Remove ignored `providerOptions` property
    As indicated by the warning:
    ```
     Vitest  "providerOptions" is ignored because it's overriden by the configs. To hide this warning, remove the "providerOptions" property from the browser configuration.
    ```

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,12 +9,6 @@ export default defineConfig({
       enabled: true,
       name: "chromium",
       provider: "playwright",
-      // workaround for https://youtrack.jetbrains.com/issue/WEB-68768/Vitest-Browser-Mode-Debugging-Doesnt-Work
-      providerOptions: {
-        launch: {
-          args: ["--remote-debugging-port=9222"],
-        },
-      },
     },
     exclude: [...configDefaults.exclude, "e2e/*"],
   },
