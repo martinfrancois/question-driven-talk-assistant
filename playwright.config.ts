72be0da3b031c3d36f3aad654c3facf71ef3d47b
François Martin <f.martin@fastmail.com>
2025-09-12 21:17:49 +0200

    docs: link bun installation

--- playwright.config.ts
+++ playwright.config.ts
@@ -81,7 +81,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run dev',
+  //   command: 'bun dev',
   //   url: 'http://localhost:5173',
   //   reuseExistingServer: !process.env.CI,
   // },
